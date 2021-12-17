07b3b79379450dd31a8610702a43d769dd5e5773
pradeepsahu5592 <89870151+pradeepsahu5592@users.noreply.github.com>
2021-12-17 12:32:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var btn = document.getElementById("btn");
+        const coupan = document.getElementById("coupan");
+        btn.addEventListener("click",()=>{
+            coupan.classList.toggle("animate");
+            
+        })
